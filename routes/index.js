b272f1073c6f1968bda10426f4d2720570f7d2dd
EduinAlv <130624709+EduinAlv@users.noreply.github.com>
2025-01-17 18:45:11 -0400

    index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ContactosController = require('./ContactosController');
 const controller = new ContactosController();
+require('dotenv').config()
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  res.render('index', { 
+    key: process.env.GOOGLEKEYPUBLIC,
+    alert: false,
+  });
 });
 
 router.post('/save',(req,res) => controller.add(req,res));
